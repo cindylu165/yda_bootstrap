6fc510c5f208c485ed2b900689820877ad350103
Lin8947 <56994144+Lin8947@users.noreply.github.com>
2021-04-13 15:08:59 +0800

    Update course_attendance.php

--- application/views/course/course_attendance.php
+++ application/views/course/course_attendance.php
@@ -1,62 +1,78 @@
-<?php $this->load->view('templates/header');?>
+<?php $this->load->view('templates/new_header');?>
+
+<div class="breadcrumb-div">
+  <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/user/index'); ?>" <?php echo $url == '/user/index' ? 'active' : ''; ?>>首頁</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="#">生涯探索課程或活動(措施B)</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/course/get_course_attendance_table_by_organization'); ?>" <?php echo $url == '/course/get_course_attendance_table_by_organization' ? 'active' : ''; ?>>課程時數清單(執行當日更新、每月自動統計報表數據)</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $title;?></li>
+    </ol>
+  </nav>
+</div>
+
 <div class="container">
   <div class="row">
-    <div class="card col s12">
-      <h4 class="card-title text-center"><?php echo $title ?></h4>
+    <div class="col-md-12">
+      <h4 class="text-dark text-center"><?php echo $title ?></h4>
       <div class="card-content">
         <form id="form" action="<?php echo site_url($url); ?>"
           method="post" accept-charset="utf-8" enctype="multipart/form-data">
           <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>" value="<?php echo $security->get_csrf_hash() ?>" />
-          <?php echo isset($error) ? '<p class="red-text text-darken-1 text-center">' . $error . '</p>' : ''; ?>
-          <?php echo isset($success) ? '<p class="green-text text-accent-4 text-center">' . $success . '</p>' : ''; ?>
+          <?php echo isset($error) ? '<p class="text-danger text-center">' . $error . '</p>' : ''; ?>
+          <?php echo isset($success) ? '<p class="text-success text-center">' . $success . '</p>' : ''; ?>
          
           <!-- course -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <select name="course" required onchange="location = this.value;" <?php echo ($hasDelegation == '0') ? 'disabled' : '' ?>>
-                <?php if (empty($courseAttendances->course)) {?>
+          <div class="col-10 m-2 mx-auto">
+            <label>課程名稱*</label>
+            <div class="input-group">
+              <select class="form-select" name="course" id="course" onchange="location = this.value;" <?php echo ($hasDelegation == '0') ? 'disabled' : '' ?>>
+              <?php if (empty($courseAttendances->course)) {?>
                   <option disabled selected value>請選擇</option>
-                  <?php }
-foreach ($courses as $i) {
-    if (!empty($courseAttendances->course)) {
-        if ($i['no'] == $courseAttendances->course) {?>
-                      <option selected value="<?php echo site_url($url . '' . $i['no'] . '/' . $startTime); ?>"><?php echo $i['course_name'] . '-' . $i['expert_name']; ?></option>
-                    <?php } else {?>
-                      <option value="<?php echo site_url($url . '' . $i['no']. '/' . $startTime); ?>"><?php echo $i['course_name']. '-' . $i['expert_name']; ?></option>
-                    <?php }
-    } else {
-        if ($i['no'] == $courseType) {?>
-                    <option selected value="<?php echo site_url($url . '' . $i['no']. '/' . $startTime); ?>"><?php echo $i['course_name']. '-' . $i['expert_name']; ?></option>
-                  <?php } else {?>
-                    <option value="<?php echo site_url($url . '' . $i['no']. '/' . $startTime); ?>"><?php echo $i['course_name']. '-' . $i['expert_name']; ?></option>
-                  <?php }}?>
-                <?php }?>
+                  <?php } foreach ($courses as $i) {
+                    if (!empty($courseAttendances->course)) {
+                      if ($i['no'] == $courseAttendances->course) {?>
+                        <option selected value="<?php echo site_url($url . '' . $i['no'] . '/' . $startTime); ?>"><?php echo $i['course_name']; ?></option>
+                      <?php } else {?>
+                        <option value="<?php echo site_url($url . '' . $i['no']. '/' . $startTime); ?>"><?php echo $i['course_name']; ?></option>
+                      <?php }
+                    } else {
+                      if ($i['no'] == $courseType) { ?>
+                        <option selected value="<?php echo site_url($url . '' . $i['no']. '/' . $startTime); ?>"><?php echo $i['course_name']; ?></option>
+                      <?php } else {?>
+                        <option value="<?php echo site_url($url . '' . $i['no']. '/' . $startTime); ?>"><?php echo $i['course_name']; ?></option>
+                      <?php }} ?>
+                  <?php }?>
               </select>
-              <label>課程名稱*</label>
             </div>
           </div>
 
           <!-- member -->
-          <h6><b>參與學員(複選)*</b></h6>
-          <div class="row">
-            <?php
-foreach ($members as $i) {?>
-              <div class="col m6">
-                <p><label>
-                  <input class="filled-in" type="checkbox" name="member[]" <?php echo ($hasDelegation == '0') ? 'disabled' : '' ?>
-                  <?php if (in_array($i['no'], $participantArray) == 1) {
-    echo "checked";
-} else {"";}?>
-                  value="<?php echo $i['no']; ?>">
+          <div class="col-10 m-2 mx-auto">
+            <h6><b>參與學員(複選)*</b></h6>
+            <div class="row">
+              <?php foreach ($members as $i) { ?>
+                <div class="col-6">
+                  <p><label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" name="member[]" <?php echo ($hasDelegation == '0') ? 'disabled' : '' ?>
+                    <?php if (in_array($i['no'], $participantArray) == 1) {
+                      echo "checked";
+                    } else {"";}?>
+                    value="<?php echo $i['no']; ?>">
 
-                  <span><?php echo $i['system_no'] . $i['name']; ?></span>
-                </label>
-                  <a href="<?php echo site_url('member/get_insurance_table_by_member/' . $i['no']); ?>" style="margin-top:0px; margin-left:25px;" class="waves-light btn-small btn-floating blue darken-4 add-btn">投保</a>
-                </p>
-
-
-              </div>
-             <?php }?>
+                    <span><?php echo $i['system_no'] . $i['name']; ?></span>
+                  </label>
+                    <a href="<?php echo site_url('member/get_insurance_table_by_member/' . $i['no']); ?>" class="btn btn-primary m-2">投保</a>
+                  </p>
+                </div>
+              <?php }?>
+            </div>
           </div>
 
           <!-- startTime -->
@@ -76,24 +92,21 @@ foreach ($members as $i) {?>
           </div>
 
           <!-- duration -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input readonly placeholder="1" type="text" id="formDuration" name="duration" min="0" step="0.25" <?php echo ($hasDelegation == '0') ? 'readonly' : '' ?> value="<?php echo (empty($courseAttendances)) ? $courseInfo ? (strtotime($courseInfo->end_time) - strtotime($courseInfo->start_time))/3600 : 0 : $courseAttendances->duration ?>">
-              <label for="formDuration">歷時*</label>
-            </div>
+          <div class="col-10 m-2 mx-auto">
+            <label for="formDuration" class="form-label">歷時*</label>
+            <input class="form-control" type="number" id="formDuration" name="duration" min="0" step="0.25" placeholder="1" <?php echo ($hasDelegation == '0') ? 'readonly' : '' ?> value="<?php echo (empty($courseAttendances)) ? $courseInfo ? (strtotime($courseInfo->end_time) - strtotime($courseInfo->start_time))/3600 : 0 : $courseAttendances->duration ?>">
           </div>
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <textarea id="note" class="materialize-textarea" required
-                name="note" <?php echo ($hasDelegation == '0') ? 'readonly' : '' ?>><?php echo (empty($courseAttendances)) ? "" : $courseAttendances->note ?></textarea>
-              <label for="work_trainning_note">備註*</label>
-            </div>
+          <div class="col-10 m-2 mx-auto">
+            <label for="work_trainning_note" class="form-label">備註*</label>
+            <textarea class="form-control" type="text" id="note" name="note" <?php echo ($hasDelegation == '0') ? 'readonly' : '' ?>><?php echo (empty($courseAttendances)) ? "" : $courseAttendances->note ?></textarea>
           </div>
 
           <?php if ($hasDelegation != '0'): ?>
           <div class="row">
-            <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" type="submit">建立</button>
+            <div class="d-grid gap-2 col-2 mx-auto">
+              <button class="btn btn-primary my-5" type="submit">建立</button>
+            </div>
           </div>
           <?php endif;?>
         </form>
@@ -172,4 +185,4 @@ foreach ($members as $i) {?>
 
 </script>
 
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
