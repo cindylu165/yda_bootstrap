f92e47c1191070bfa30e745ebdd719b4c4f19352
Lin8947 <56994144+Lin8947@users.noreply.github.com>
2021-04-13 15:17:36 +0800

    Update company.php

--- application/views/work/company.php
+++ application/views/work/company.php
@@ -1,111 +1,113 @@
-<?php $this->load->view('templates/header');?>
+<?php $this->load->view('templates/new_header');?>
+
+<div class="breadcrumb-div">
+  <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/user/index'); ?>" <?php echo $url == '/user/index' ? 'active' : ''; ?>>首頁</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="#">工作體驗(措施C)</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/work/get_company_table_by_organization'); ?>" <?php echo $url == '/work/get_company_table_by_organization' ? 'active' : ''; ?>>店家參考清單(歷年資料)</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $title;?></li>
+    </ol>
+  </nav>
+</div>
+
 <div class="container">
   <div class="row">
-    <div class="card col s12">
-      <h4 class="card-title text-center"><?php echo $title ?></h4>
+    <div class="col-md-12">
+      <h4 class="text-dark text-center"><?php echo $title ?></h4>
       <div class="card-content">
         <form action="<?php echo site_url($url);?>" 
           method="post" accept-charset="utf-8" enctype="multipart/form-data">
           <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>" value="<?php echo $security->get_csrf_hash() ?>" />
 
-          <?php echo isset($error) ? '<p class="red-text text-darken-3 text-center">'.$error.'</p>' : '';?>
-          <?php echo isset($success) ? '<p class="green-text text-darken-3 text-center">'.$success.'</p>' : '';?>
+          <?php echo isset($error) ? '<p class="text-danger text-center">'.$error.'</p>' : '';?>
+          <?php echo isset($success) ? '<p class="text-success text-center">'.$success.'</p>' : '';?>
 
           <!-- name -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input required type="text" id="name" name="name" placeholder="範例-汽車維修廠" value="<?php echo (empty($companys)) ? "" : $companys->name ?>">
-              <label for="name">店家名稱*</label>
-            </div> 
+          <div class="col-8 m-2 mx-auto">
+            <label for="name" class="form-label">店家名稱*</label>
+            <input class="form-control" type="text" id="name" name="name" placeholder="範例-汽車維修廠" required value="<?php echo (empty($companys)) ? "" : $companys->name ?>">
           </div>
 
           <!-- boss_name -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input required type="text" id="boss_name" name="boss_name" placeholder="OOO" value="<?php echo (empty($companys)) ? "" : $companys->boss_name ?>">
-              <label for="boss_name">老闆名稱*</label>
-            </div> 
+          <div class="col-8 m-2 mx-auto">
+            <label for="boss_name" class="form-label">老闆名稱*</label>
+            <input class="form-control" type="text" id="boss_name" name="boss_name" placeholder="OOO" required value="<?php echo (empty($companys)) ? "" : $companys->boss_name ?>">
           </div>
 
           <!-- phone -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input type="text" id="phone" name="phone" placeholder="OOOO-OOO-OOO" value="<?php echo (empty($companys)) ? "" : $companys->phone ?>">
-              <label for="phone">連絡電話</label>
-            </div> 
+          <div class="col-8 m-2 mx-auto">
+            <label for="phone" class="form-label">連絡電話</label>
+            <input class="form-control" type="text" id="phone" name="phone" placeholder="OOOO-OOO-OOO" value="<?php echo (empty($companys)) ? "" : $companys->phone ?>">
           </div>
 
-           <!-- category -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <select name="category" required>
-                <?php if(empty($companys->category)){?>
-                  <option disabled selected value>請選擇</option>
-                  <?php }foreach($categorys as $i) { 
-                  if(!empty($companys->category)){
-                    if($i['no'] == $companys->category){ ?>
-                      <option selected value="<?php echo $i['no'];?>"><?php echo $i['content'];?></option>
-                    <?php }
-                    else{ ?>
-                      <option value="<?php echo $i['no'];?>"><?php echo $i['content'];?></option>
-                    <?php }
-                  }else{ ?>
-                    <option value="<?php echo $i['no'];?>"><?php echo $i['content'];?></option>
-                  <?php } ?>
-                <?php } ?>
-              </select>
-              <label>工作類別*</label>
-            </div>
+          <!-- category -->
+          <div class="col-8 m-2 mx-auto">
+            <label class="form-label" for="category">工作類別*</label>
+            <select class="form-select" name="category" id="category">
+            <?php if (empty($companys->category)) {?>
+              <option disabled selected value>請選擇</option>
+            <?php } ?>
+            <?php foreach ($categorys as $i) {
+                    if (!empty($companys->category)) {
+                      if ($i['no'] == $companys->category) { ?>
+                        <option selected value="<?php echo $i['no']; ?>"><?php echo $i['content']; ?></option>
+                <?php } else {?>
+                        <option value="<?php echo $i['no']; ?>"><?php echo $i['content']; ?></option>
+                <?php }
+                    } else {?>
+                        <option value="<?php echo $i['no']; ?>"><?php echo $i['content']; ?></option>
+              <?php }?>
+            <?php }?>
+            </select>
           </div>
 
           <!-- content -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <textarea id="content" class="materialize-textarea" placeholder="協助汽修廠業務" name="content"><?php echo (empty($companys)) ? "" : $companys->content ?></textarea>
-              <label for="content">工作內容:</label>
-            </div>
+          <div class="col-8 m-2 mx-auto">
+            <label for="work_content" class="form-label">工作內容:</label>
+            <textarea class="form-control" type="text" id="work_content" name="work_content" placeholder="協助汽修廠業務" value="<?php echo (empty($companys)) ? "" : $companys->content ?>"></textarea>
           </div>
 
           <!-- requirement -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <textarea id="requirement" class="materialize-textarea" placeholder="可配合到晚班( 約到21點)、不會遲到早退" name="requirement"><?php echo (empty($companys)) ? "" : $companys->requirement ?></textarea>
-              <label for="requirement">工作條件:</label>
-            </div>
+          <div class="col-8 m-2 mx-auto">
+            <label for="requirement" class="form-label">工作條件:</label>
+            <textarea class="form-control" type="text" id="requirement" name="requirement" placeholder="可配合到晚班( 約到21點)、不會遲到早退" value="<?php echo (empty($companys)) ? "" : $companys->requirement ?>"></textarea>
           </div>
 
           <!-- address -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <textarea id="address" class="materialize-textarea" placeholder="OOOOOOOOOO" name="address"><?php echo (empty($companys)) ? "" : $companys->address ?></textarea>
-              <label for="address">工作地址:</label>
-            </div>
+          <div class="col-8 m-2 mx-auto">
+            <label for="address" class="form-label">工作地址:</label>
+            <textarea class="form-control" type="text" id="address" name="address" placeholder="OOOOOOOOOO" value="<?php echo (empty($companys)) ? "" : $companys->address ?>"></textarea>
           </div>
 
           <!-- is_open -->
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <select name="is_open">
-                <?php if(isset($companys->is_open)){
-                  if($companys->is_open == "1"){?>
+          <div class="col-8 m-2 mx-auto">
+            <label class="form-label" for="is_open">是否願意開放資料給全國夥伴</label>
+            <select class="form-select" name="is_open" id="is_open">
+              <?php if(isset($companys->is_open)) {
+                if($companys->is_open == "1") { ?>
                   <option value="1" selected>是</option>
-                  <option value="0" >否</option>
-                <?php }else{?>
-                  <option value="1" >是</option>
+                  <option value="0">否</option>
+                <?php } else { ?>
+                  <option value="1">是</option>
                   <option value="0" selected>否</option>
                 <?php }
-                }else{?>
+                } else { ?>
                   <option disabled selected value>請選擇</option>
                   <option value="1">是</option>
                   <option value="0">否</option>
-                <?php }?>
-              </select>
-              <label>是否願意開放資料給全國夥伴</label>
-            </div>
+                <?php } ?>
+            </select>
           </div>
          
-          <div class="row">
-            <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" type="submit">送出</button>
+          <div class="d-grid gap-2 col-2 mx-auto">
+            <button class="btn btn-primary m-3" type="submit">送出</button>
           </div>
         </form>
       </div>
@@ -119,4 +121,4 @@
   
 </script>
 
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
