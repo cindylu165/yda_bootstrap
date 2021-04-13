34b527523bf888d41b589e38c717567cab290931
Lin8947 <56994144+Lin8947@users.noreply.github.com>
2021-04-13 15:16:07 +0800

    Update month_review_table.php

--- application/views/member/month_review_table.php
+++ application/views/member/month_review_table.php
@@ -1,13 +1,33 @@
-<?php $this->load->view('templates/header');?>
+<?php $this->load->view('templates/new_header');?>
+
+<div class="breadcrumb-div">
+  <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/user/index'); ?>" <?php echo $url == '/user/index' ? 'active' : ''; ?>>首頁</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="#">輔導會談(措施A)</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/member/get_member_table_by_counselor'); ?>" <?php echo $url == '/member/get_member_table_by_counselor' ? 'active' : ''; ?>>開案學員清單</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $title;?></li>
+    </ol>
+  </nav>
+</div>
+
 <div class="container">
   <div class="row">
-    <div class="card col s12">
-      <h4 class="card-title text-center"><?php echo $title ?></h4>
+    <div class="col-md-12">
+      <h4 class="text-dark text-center"><?php echo $title ?></h4>
       <?php if($hasDelegation != '0'): ?>
-      <a class="btn col s2 offset-s5 waves-effect blue lighten-1" href="<?php echo site_url($url);?>">新增</a>
+        <div class="d-grid gap-2 col-2 mx-auto">
+          <a class="btn btn-info m-3" href="<?php echo site_url($url);?>">新增</a>
+        </div>
       <?php endif;?>
       <div class="card-content">
-        <table class="highlight">
+        <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
               <th scope="col">日期</th>
@@ -25,7 +45,8 @@
                   }
                 }?></td>
                 <td>
-                  <a class="btn waves-effect blue darken-4" href="<?php echo site_url('member/month_review/'.$i['member'].'/'.$i['no']);?>">查看/修改</a>                         
+                  <a class="btn btn-info m-2" href="<?php echo site_url('member/month_review/'.$i['member'].'/'.$i['no']);?>">查看/修改</a>
+                  <a class="btn btn-warning" href="<?php echo site_url('member/delete_review_table?no=' . $i['no']); ?>">刪除</a>
                 </td>
               </tr>
             <?php } ?>
@@ -35,4 +56,4 @@
     </div>
   </div>
 </div>
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
