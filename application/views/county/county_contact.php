6ca183511dfe94c5a9c326911795521f5e4b9a80
Cindy Lu <cindylu242@gmail.com>
2021-04-20 18:24:59 +0800

    update_county_contact

--- application/views/county/county_contact.php
+++ application/views/county/county_contact.php
@@ -1,61 +1,50 @@
-<?php $this->load->view('templates/header');?>
+<?php $this->load->view('templates/new_header');?>
 <div class="container">
-  <div class="row">
-    <div class="card col s12">
+  <div class="col-md-12">
       <h4 class="card-title text-center"><?php echo $title ?></h4>
       <div class="card-content">
-        <form action="<?php echo site_url($url); ?>"
+        <form class="row g-3" action="<?php echo site_url($url); ?>"
           method="post" accept-charset="utf-8" enctype="multipart/form-data">
           <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>" value="<?php echo $security->get_csrf_hash() ?>" />
-          <?php echo isset($error) ? '<p class="red-text text-darken-3 text-center">' . $error . '</p>' : ''; ?>
-          <?php echo isset($success) ? '<p class="green-text text-darken-3 text-center">' . $success . '</p>' : ''; ?>
+          <?php echo isset($error) ? '<p class="text-danger text-center">' . $error . '</p>' : ''; ?>
+          <?php echo isset($success) ? '<p class="text-success text-center">' . $success . '</p>' : ''; ?>
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input type="text" id="name" name="name" required
-                value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->name ?>">
+          <div class="col-12">
               <label for="name">姓名*</label>
-            </div>
+              <input class="form-control" type="text" id="name" name="name" required
+                value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->name ?>">
           </div>
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input type="text" id="title" name="title" required
+          <div class="col-12">
+            <label for="title">職稱*</label>
+            <input class="form-control" type="text" id="title" name="title" required
                 value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->title ?>">
-              <label for="title">職稱*</label>
-            </div>
           </div>
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input type="text" id="orgnizer" name="orgnizer" required
+          <div class="col-12">
+            <label for="orgnizer">承辦單位*</label>
+            <input  class="form-control"type="text" id="orgnizer" name="orgnizer" required
                 value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->orgnizer ?>">
-              <label for="orgnizer">承辦單位*</label>
-            </div>
           </div>
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input type="text" id="phone" name="phone" required
-                value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->phone ?>">
-              <label for="phone">聯絡電話*</label>
-            </div>
+          <div class="col-12">
+            <label for="phone">聯絡電話*</label>
+            <input class="form-control" type="text" id="phone" name="phone" required
+              value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->phone ?>">
           </div>
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <input type="text" id="email" name="email" required
+          <div class="col-12">
+            <label for="email">電子郵件*</label>
+            <input class="form-control" type="text" id="email" name="email" required
                 value="<?php echo (empty($countyContacts)) ? "" : $countyContacts->email ?>">
-              <label for="email">電子郵件*</label>
-            </div>
           </div>
 
-          <div class="row">
-            <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" type="submit">送出</button>
+          <div class="col-12 text-center">
+          <button class="btn btn-primary m-3" type="submit">送出</button>
+            <!-- <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" type="submit">送出</button> -->
           </div>
         </form>
       </div>
-    </div>
   </div>
 </div>
 
@@ -65,4 +54,4 @@
 
 </script>
 
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
