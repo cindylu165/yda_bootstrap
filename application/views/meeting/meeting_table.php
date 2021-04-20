c0c82c8567c082998c7c3378b509cbec9440b184
Cindy Lu <cindylu242@gmail.com>
2021-04-21 02:14:56 +0800

    update_meeting_table

--- application/views/meeting/meeting_table.php
+++ application/views/meeting/meeting_table.php
@@ -1,28 +1,29 @@
-<?php $this->load->view('templates/header');?>
-<div class="container">
-  <div class="row">
-    <div class="card col s12">
+<?php $this->load->view('templates/new_header'); ?>
+<div class="container" style="width:100%;">
+	<div class="row">
+  <div class="card-body col-sm-12">
       <h4 class="card-title text-center"><?php echo $title ?></h4>
       <?php if ($hasDelegation != '0'): ?>
-      <a class="btn col s2 offset-s5 waves-effect blue lighten-1" href="<?php echo site_url($url); ?>">新增</a>
+      <div class="col-md-12" style="text-align:center;">
+        <a class="btn btn-info" href="<?php echo site_url($url); ?>">新增</a>
+        </div>
       <?php endif;?>
       <div class="card-content">
+      
         <!-- years -->
-				<div class="row">
-          <div class="input-field col s10 offset-m2 m8">
-            <select name="years" id="years" onchange="location = this.value;">
+        <div class="row justify-content-center">
+					<div class="col-sm-10 col-md-8">
+						<label>年度</label>
+						<select class="form-select form-select-lg mb-3" name="years" id="years" onchange="location = this.value;">
+							<?php foreach ($years as $i) { ?>
+								<<option <?php echo ($yearType == $i['year']) ? 'selected' : '' ?> value="<?php echo site_url('/meeting/meeting_table/' . $i['year']); ?>"><?php echo $i['year'] ?></option>
+							<?php } ?>
+						</select>
 
-						<?php foreach ($years as $i) {?>
+					</div>
+				</div>
 
-							<option <?php echo ($yearType == $i['year']) ? 'selected' : '' ?> value="<?php echo site_url('/meeting/meeting_table/' . $i['year']); ?>"><?php echo $i['year'] ?></option>
-            <?php }?>
-
-            </select>
-            <label>年度</label>
-          </div>
-        </div>
-
-        <table class="responsive-table highlight centered">
+        <table class="table table-hover align-middle text-center">
           <thead class="thead-dark">
             <tr>
               <th scope="col">會議名稱</th>
@@ -36,8 +37,8 @@
                 <td><?php echo $i['title']; ?></td>
                 <td><?php echo $i['start_time']; ?></td>
                 <td>
-                  <a class="btn waves-effect blue lighten-1" href="<?php echo site_url($url . $i['no'] . '/' . $i['year']); ?>">查看/修改</a>
-                  <a class="btn waves-effect orange darken-2" href="<?php echo site_url('meeting/delete?no=' . $i['no']); ?>">刪除</a>
+                  <a class="btn btn-info" href="<?php echo site_url($url . $i['no'] . '/' . $i['year']); ?>">查看/修改</a>
+                  <a class="btn btn-warning" href="<?php echo site_url('meeting/delete?no=' . $i['no']); ?>">刪除</a>
                 </td>
               </tr>
             <?php }?>
@@ -47,4 +48,4 @@
     </div>
   </div>
 </div>
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
