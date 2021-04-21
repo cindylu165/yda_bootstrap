0b1c36030070cabfe38de39138af433685880423
Lin8947 <56994144+Lin8947@users.noreply.github.com>
2021-04-21 15:48:23 +0800

    Update review_table.php

--- application/views/review/review_table.php
+++ application/views/review/review_table.php
@@ -1,12 +1,12 @@
-<?php $this->load->view('templates/header');?>
+<?php $this->load->view('templates/new_header');?>
 <div class="container">
   <div class="row">
-    <div class="card col s12">
-      <h4 class="card-title text-center"><?php echo $title ?></h4>
+    <div class="col-md-12">
+      <h4 class="text-dark text-center"><?php echo $title ?></h4>
 
-      <h6 class="card-title text-center">等待批准清單</h6>
+      <h6 class="text-dark text-center">等待批准清單</h6>
       <div class="card-content">
-        <table class="highlight centered">
+        <table class="table table-hover">
           <thead class="thead-dark"></thead>
             <tr>
               <th scope="col">日期</th>
@@ -28,7 +28,7 @@
 }?></td>
               <td><?php echo $value['end_time']; ?></td>
               <td>
-                <a class="btn waves-effect blue lighten-1" href="<?php echo site_url($url . '/' . $value['no']); ?>">審核</a>
+                <a class="btn btn-primary my-5" href="<?php echo site_url($url . '/' . $value['no']); ?>">審核</a>
               </td>
             </tr>
             <?php }?>
@@ -36,9 +36,9 @@
         </table>
       </div>
 
-      <h6 class="card-title text-center">已批准清單</h6>
+      <h6 class="text-dark text-center">已批准清單</h6>
       <div class="card-content">
-        <table class="highlight">
+        <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
               <th scope="col">日期</th>
@@ -54,13 +54,14 @@
               <td><?php echo $value['create_time']; ?></td>
               <td><?php if ($value['form_name'] == 'case_assessment'): echo '更換輔導員申請';elseif ($value['form_name'] == 'counselor_users'): echo '輔導員帳號申請';elseif ($value['form_name'] == 'update_usable'): echo '輔導員帳號停用申請';elseif ($value['form_name'] == 'end_youth'):echo '青少年結案申請';elseif($value['form_name'] == 'reopen_youth'): echo '青少年復案申請';elseif($value['form_name'] == 'transfer_youth') : echo '青少年轉介申請'; endif;?></td>
               <td><?php foreach ($statuses as $i) {
-    if ($i['no'] == $value['status']) {
-        echo $i['content'];
-    }
-}?></td>
+                  if ($i['no'] == $value['status']) {
+                    echo $i['content'];
+                  }
+                } ?>
+              </td>
               <td><?php echo $value['end_time']; ?></td>
               <td>
-                <a class="btn waves-effect blue lighten-1" href="<?php echo site_url($url . '/' . $value['no']); ?>">審核</a>
+                <a class="btn btn-primary my-5" href="<?php echo site_url($url . '/' . $value['no']); ?>">審核</a>
               </td>
             </tr>
             <?php }?>
@@ -68,9 +69,9 @@
         </table>
       </div>
 
-      <h6 class="card-title text-center">未批准清單</h6>
+      <h6 class="text-dark text-center">未批准清單</h6>
       <div class="card-content">
-        <table class="highlight">
+        <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
               <th scope="col">日期</th>
@@ -86,13 +87,14 @@
               <td><?php echo $value['create_time']; ?></td>
               <td><?php if ($value['form_name'] == 'case_assessment'): echo '更換輔導員申請';elseif ($value['form_name'] == 'counselor_users'): echo '輔導員帳號申請';elseif ($value['form_name'] == 'update_usable'): echo '輔導員帳號停用申請';elseif ($value['form_name'] == 'end_youth'):echo '青少年結案申請';elseif($value['form_name'] == 'reopen_youth'): echo '青少年復案申請';elseif($value['form_name'] == 'transfer_youth') : echo '青少年轉介申請'; endif;?></td>
               <td><?php foreach ($statuses as $i) {
-    if ($i['no'] == $value['status']) {
-        echo $i['content'];
-    }
-}?></td>
+                  if ($i['no'] == $value['status']) {
+                    echo $i['content'];
+                  }
+                } ?>
+              </td>
               <td><?php echo $value['end_time']; ?></td>
               <td>
-                <a class="btn waves-effect blue lighten-1" href="<?php echo site_url($url . '/' . $value['no']); ?>">審核</a>
+                <a class="btn btn-primary my-5" href="<?php echo site_url($url . '/' . $value['no']); ?>">審核</a>
               </td>
             </tr>
             <?php }?>
@@ -106,4 +108,4 @@
 
   </div>
 </div>
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
