15918a68ebe0bc26a441cf70128f9192dce9a396
Cindy Lu <cindylu242@gmail.com>
2021-04-28 01:29:43 +0800

    find_no_path

--- application/views/report/high_school_trend_survey_count_report.php
+++ application/views/report/high_school_trend_survey_count_report.php
@@ -1,17 +1,35 @@
-<?php $this->load->view('templates/header');?>
-<div class="container" style="width:90%;">
-  <div class="row">
-    <div class="card col s12">
-      <div class="row"> 
+<?php $this->load->view('templates/new_header');?>
+
+<div class="breadcrumb-div">
+  <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/user/index'); ?>" <?php echo $url == '/user/index' ? 'active' : ''; ?>>首頁</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="#">報表管理</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/report/counseling_member_count_report_table'); ?>" <?php echo $url == '/report/counseling_member_count_report_table' ? 'active' : ''; ?>>每月執行進度表清單</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $title;?></li>
+    </ol>
+  </nav>
+</div>
+
+<div class="container" style="width:100%;">
+	<div class="row">
+		<div class="card-body col-sm-12">
+      <!-- <div class="row"> 
         <a class="btn col s2 offset-s0 waves-effect teal darken-2" style="margin:10px;" href="<?php echo site_url('/report/counseling_member_count_report_table/' . $yearType . '/' . $monthType); ?>">←每月執行進度表清單</a>
-      </div>
+      </div> -->
       <h4 class="card-title text-center"><?php echo $title ?></h4>
       <h6 class="card-title text-center"><?php echo '民國'  . $yearType . '年' . $monthType . '月'; ?></h6>
 
       <div class="card-content">
         <form action="<?php echo site_url($url); ?>"
           method="post" accept-charset="utf-8" enctype="multipart/form-data">
-          <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>"
+          <input class="form-control" type="hidden" name="<?php echo $security->get_csrf_token_name() ?>"
             value="<?php echo $security->get_csrf_hash() ?>" />
 
           <?php echo isset($error) ? '<p class="red-text text-darken-1 text-center">' . $error . '</p>' : ''; ?>
@@ -40,8 +58,8 @@
           <?php if($reportLogs) :?>
               <h5 class="text-center"><?php echo '審核流程' ?></h5>
 
-              <table class="highlight centered" style="border:2px grey solid;">
-                <thead class="thead-dark">
+              <table class="table table-hover table-bordered align-middle text-center">
+								<thead class="header">
                   <tr>
                     <th scope="col">使用者姓名</th>
                     <th scope="col">時間</th>
@@ -75,13 +93,17 @@
 
             <?php if($reportProcessesCounselorStatus != $reviewStatus['review_process_pass']) :?>
         
-          <div class="row">
-            <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" name="temp" value="Temp" type="submit">暫存</button>
-          </div>
+              <div class="row justify-content-center">
+                <div class="d-grid gap-2 col-sm-6 col-md-4 mb-3">
+                  <button class="btn btn-primary" type="submit">暫存</button>
+                </div>
+              </div>
 
-          <div class="row">
-            <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" name="save" value="Save" type="submit">送出</button>
-          </div>
+              <div class="row justify-content-center">
+                <div class="d-grid gap-2 col-sm-6 col-md-4 mb-3">
+                  <button class="btn btn-primary" name="save" value="Save" type="submit">送出</button>
+                </div>
+              </div>
 
           <?php endif;?>
 
@@ -90,4 +112,4 @@
     </div>
   </div>
 </div>
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
