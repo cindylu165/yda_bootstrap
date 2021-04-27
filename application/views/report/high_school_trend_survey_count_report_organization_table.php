5c8f28e53ea264afc01a167e97e1d53455bd6d4b
Cindy Lu <cindylu242@gmail.com>
2021-04-28 01:23:22 +0800

    find_no_path

--- application/views/report/high_school_trend_survey_count_report_organization_table.php
+++ application/views/report/high_school_trend_survey_count_report_organization_table.php
@@ -1,10 +1,28 @@
-<?php $this->load->view('templates/header');?>
-<div class="container" style="width:90%;">
-  <div class="row">
-    <div class="card col s12">
-      <div class="row"> 
+<?php $this->load->view('templates/new_header');?>
+
+<div class="breadcrumb-div">
+  <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/user/index'); ?>" <?php echo $url == '/user/index' ? 'active' : ''; ?>>首頁</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="#">報表管理</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/report/counseling_member_count_report_table'); ?>" <?php echo $url == '/report/counseling_member_count_report_table' ? 'active' : ''; ?>>每月執行進度表清單</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $title;?></li>
+    </ol>
+  </nav>
+</div>
+
+<div class="container" style="width:100%;">
+	<div class="row">
+		<div class="card-body col-sm-12">
+      <!-- <div class="row"> 
         <a class="btn col s2 offset-s0 waves-effect teal darken-2" style="margin:10px;" href="<?php echo site_url('/report/counseling_member_count_report_table/' . $yearType . '/' . $monthType); ?>">←每月執行進度表清單</a>
-      </div>
+      </div> -->
       <h4 class="card-title text-center"><?php echo $title ?></h4>
       <h6 class="card-title text-center"><?php echo '民國'  . $yearType . '年' . $monthType . '月'; ?></h6>
       <div class="card-content">
@@ -13,40 +31,37 @@
           method="post" accept-charset="utf-8" enctype="multipart/form-data">
         <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>"
             value="<?php echo $security->get_csrf_hash() ?>" />
-
         <!-- years -->
-        <div class="row">
-          <div class="input-field col s10 offset-m2 m8">
-            <select name="years" id="years" onchange="location = this.value;">
-
-              <?php foreach($years as $i) {?>
-
-              <option <?php echo ($yearType == ($i['year'])) ? 'selected' : ''?>
-                value="<?php echo site_url('/report/high_school_trend_survey_count_report_organization_table/' . $i['year'] . '/' . $monthType);?>"><?php echo $i['year'] ?></option>
-              <?php } ?>
-
-            </select>
+        <div class="row justify-content-center">
+          <div class="col-sm-10 col-md-8">
             <label>年度</label>
+            <select class="form-select form-select-lg mb-3" name="years" id="years" onchange="location = this.value;">
+            
+              <?php foreach ($years as $i) { ?>
+                <option <?php echo ($yearType == ($i['year'])) ? 'selected' : ''?>
+                  value="<?php echo site_url('/report/high_school_trend_survey_count_report_organization_table/' . $i['year'] . '/' . $monthType);?>"><?php echo $i['year'] ?></option>
+              <?php } ?>
+            </select>
           </div>
         </div>
-
+        
         <!-- months -->
-        <div class="row">
-          <div class="input-field col s10 offset-m2 m8">
-            <select name="months" id="months" onchange="location = this.value;">
-
-              <?php foreach($months as $i) {?>
-
-              <option <?php echo ($monthType == $i) ? 'selected' : ''?>
+        <div class="row justify-content-center">
+          <div class="col-sm-10 col-md-8">
+            <label>月份</label>
+            <select class="form-select form-select-lg mb-3" name="months" id="months" onchange="location = this.value;">
+              
+              <?php foreach ($months as $i) { ?>
+              
+                <option <?php echo ($monthType == $i) ? 'selected' : '' ?> 
                 value="<?php echo site_url('/report/high_school_trend_survey_count_report_organization_table/' . $yearType . '/' . $i);?>"><?php echo $i ?></option>
               <?php } ?>
 
             </select>
-            <label>月份</label>
           </div>
         </div>
 
-        <a class="btn waves-effect" href="<?php echo site_url('export/organization_month_report_export/' . 'HighSchoolTrendSurveyCountReport' . '/' . $yearType . '/' . $monthType); ?>">列印(下載EXCEL檔)</a><br/><br/>
+        <a class="btn btn-success" href="<?php echo site_url('export/organization_month_report_export/' . 'HighSchoolTrendSurveyCountReport' . '/' . $yearType . '/' . $monthType); ?>">列印(下載EXCEL檔)</a><br/><br/>
 
         <br/><br/>
 
@@ -65,7 +80,7 @@
         <?php if ($reportLogs): ?>
                 <h5 class="text-center"><?php echo '審核流程' ?></h5>
 
-                <table class="highlight centered" style="border:2px grey solid;">
+                <table class="table table-hover text-center" style="border:2px grey solid;">
                   <thead class="thead-dark">
                     <tr>
                       <th scope="col">使用者姓名</th>
@@ -100,8 +115,9 @@
         <?php if ($reportProcessesCountyStatus == $reviewStatus['review_process_wait'] && $role == 3): ?>
 
           <div class="row">
-              <div class="input-field col s10 offset-m2 m8">
-                <select name="reviewStatus">
+            <div class="input-field col-10 m-2 mx-auto">
+              <label>審核</label>
+                <select class="form-select" name="reviewStatus">
                   <?php if ($reportProcessesCountyStatus == $reviewStatus['review_process_wait']) {?>
                   <option disabled selected value>請選擇</option>
                   <?php }
@@ -120,17 +136,15 @@
             <?php } }?>
                   <?php }?>
                 </select>
-                <label>審核</label>
               </div>
             </div>
 
 
 
-          <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
-              <textarea required id="formComment" class="materialize-textarea" placeholder=""
-                name="comment"></textarea>
-              <label for="formComment">備註*:</label>
+                   <div class="row">
+            <div class="input-field col-10 m-2 mx-auto">
+              <label for="formComment" class="form-label">備註*:</label>
+              <textarea class="form-control" required id="formComment" name="comment" placeholder=""></textarea>
             </div>
           </div>
 
@@ -160,8 +174,10 @@
           </div> -->
 
           <div class="row">
-            <button class="btn waves-effect col s6 offset-m4 m4 blue darken-4" type="submit">送出</button>
-          </div>
+              <div class="d-grid gap-2 col-2 mx-auto">
+                <button class="btn btn-primary my-5" type="submit">送出</button>
+              </div>
+            </div>
         </form>
 
         <?php endif;?>
@@ -227,4 +243,4 @@
     }
 
 </script>
-<?php $this->load->view('templates/footer');?>
+<?php $this->load->view('templates/new_footer');?>
