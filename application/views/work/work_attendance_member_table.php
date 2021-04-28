8807a6486e70c708864a608dccf495230ac8c224
Lin8947 <56994144+Lin8947@users.noreply.github.com>
2021-04-28 15:28:25 +0800

    Update work_attendance_member_table.php

--- application/views/work/work_attendance_member_table.php
+++ application/views/work/work_attendance_member_table.php
@@ -4,30 +4,29 @@
     <div class="col-md-12">
       <h4 class="text-dark text-center"><?php echo $title ?></h4>
       <?php if($hasDelegation != '0' && $canInsert != '0'): ?>
+
         <div class="d-grid gap-2 col-2 mx-auto">
           <a class="btn btn-info m-3" href="<?php echo site_url($url);?>">新增</a>
         </div>
-      <?php endif;?>
+
+      <?php endif; ?>
       <div class="card-content">
 
         <!-- years -->
-				<div class="row">
-          <div class="input-field col s10 offset-m2 m8">
-            <select name="years" id="years" onchange="location = this.value;">
-		
-						<?php foreach($years as $i) {?>
-            
-							<option <?php echo ($yearType == $i['year']) ? 'selected' : ''?> value="<?php echo site_url('/work/get_work_attendance_table_by_organization/' .  $i['year']);?>"><?php echo $i['year']?></option>
-            <?php } ?>
-
-            </select>
+        <div class="row justify-content-center">
+          <div class="col-sm-10 col-md-8 mb-3">
             <label>年度</label>
+            <select class="form-select form-select-lg" name="years" id="years" onchange="location = this.value;">
+              <?php foreach ($years as $i) { ?>
+                <option <?php echo ($yearType == ($i['year'])) ? 'selected' : '' ?> value="<?php echo site_url('/work/get_work_attendance_table_by_organization/' .  $i['year']);?>"><?php echo $i['year']?></option>
+              <?php } ?>
+            </select>
           </div>
         </div>
 
-        <a id="print" class="btn waves-effect">列印</a>   
+        <a id="print" class="btn btn-success">列印</a>
 
-        <table class="highlight centered">
+        <table class="table table-hover text-center">
           <thead class="thead-dark">
             <tr>
               <th scope="col">店家名稱</th>
@@ -43,7 +42,7 @@
                 <td><?php echo $i['start_time'];?></td>
                 <td><?php echo $i['youthName'];?></td>
                 <td>
-                  <a class="btn waves-effect blue lighten-1" href="<?php echo site_url($url .$i['work_experience'] . '/' .$i['no']);?>">查看/修改</a>   
+                  <a class="btn btn-info mx-2" href="<?php echo site_url($url .$i['work_experience'] . '/' .$i['no']);?>">查看/修改</a>
                 </td>
               </tr>
             <?php } ?>
