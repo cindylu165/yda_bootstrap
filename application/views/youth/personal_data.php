3f1d3ff3091ae53a5f7a78212818f9e0ce2e8181
Cindy Lu <cindylu242@gmail.com>
2021-05-02 15:26:54 +0800

    check_ok

--- application/views/youth/personal_data.php
+++ application/views/youth/personal_data.php
@@ -1,7 +1,32 @@
 <?php $this->load->view('templates/new_header')?>
-<div class="container" style="width:90%;">
-  <div class="row">
-      <div class="row">
+<div class="breadcrumb-div">
+  <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="<?php echo site_url('/user/index'); ?>" <?php echo $url == '/user/index' ? 'active' : ''; ?>>首頁</a>
+      </li>
+      <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+        <a href="#">評估開案</a>
+      </li>
+      <?php if ($comePage == "allSource"): ?>
+        <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+          <a href="<?php echo site_url('/youth/get_all_source_youth_table'); ?>">原始來源清單</a>
+        </li>
+      <?php elseif ($comePage == "allYouth"): ?>
+        <li class="breadcrumb-item active" style="color:blue;" aria-current="page">
+          <a href="<?php echo site_url('/youth/get_all_youth_table'); ?>">需關懷追蹤青少年清單</a>
+        </li>
+      <?php endif;?>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo $title;?></li>
+    
+    </ol>
+  </nav>
+</div>
+
+<div class="container" style="width:100%;">
+	<div class="row">
+		<div class="card-body col-sm-12">
+      <!-- <div class="row">
         <?php if ($comePage == "allSource"): ?>
           <div class="col-md-12" style="text-align:left;">
           <a class="btn btn-warning" style="margin:10px;" href="<?php echo site_url('/youth/get_all_source_youth_table'); ?>">←原始來源清單</a>
@@ -9,9 +34,9 @@
           <div class="col-md-12" style="text-align:left;">
           <a class="btn btn-warning" style="margin:10px;" href="<?php echo site_url('/youth/get_all_youth_table'); ?>">←需關懷追蹤青少年清單</a>
         <?php endif;?>
-      </div>
+      </div> -->
       <h4 class="card-title text-center"><?php echo $title ?></h4>
-      <div class="col-md-12">
+      <div class="card-content">
         <form action="<?php echo site_url($url); ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
           <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>"
             value="<?php echo $security->get_csrf_hash() ?>" />
@@ -20,7 +45,7 @@
 
           <!-- identification -->
         <div class="row">
-          <div class="col">
+          <div class="col-10 m-2 mx-auto">
             <label for="formIdentification">身分證</label>
             <input class="form-control" type="text" id="formIdentification" name="identification" placeholder="A000000000"
               value="<?php echo (empty($youths)) ? "" : $youths->identifications ?>"
@@ -28,7 +53,7 @@
           </div>
         
         <!-- name -->
-          <div class="col">
+          <div class="col-10 m-2 mx-auto">
             <label for="formName">姓名*</label>
             <input class="form-control" type="text" id="formName" name="name" placeholder="范立人" required
               value="<?php echo (empty($youths)) ? "" : $youths->name ?>"
@@ -38,7 +63,7 @@
 
     <!-- birth -->
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formBirth">出生年月日</label>
         <input class="form-control" type="text" id="formBirth" name="birth" class="datepicker"
           value="<?php echo (empty($youths)) ? "" : $youths->birth ?>"
@@ -46,7 +71,7 @@
       </div>
 
     <!-- gender -->
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label>性別</label>
         <select class="form-select" name="gender" <?php echo (empty($youths->gender)) ? "" : "" ?>>
           <?php if (empty($youths->gender)) {?>
@@ -65,7 +90,7 @@
       </div>
 
     <!-- phone -->
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formPhone">電話</label>
         <input class="form-control" type="text" id="formPhone" name="phone" placeholder="0900-000-000"
           value="<?php echo (empty($youths)) ? "" : $youths->phone ?>"
@@ -75,7 +100,7 @@
 
     <!-- householdAddress -->
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formHouseholdAddress">戶籍地址</label>
         <input class="form-control" type="text" id="formHouseholdAddress" name="householdAddress" placeholder="個案地址"
           value="<?php echo (empty($youths)) ? "" : $youths->household_address_aes ?>"
@@ -85,7 +110,7 @@
 
     <!-- resideAddress -->
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formResideAddress">居住地址</label>
         <input class="form-control" type="text" id="formResideAddress" name="resideAddress" placeholder="個案地址"
           value="<?php echo (empty($youths)) ? "" : $youths->reside_address_aes ?>"
@@ -93,12 +118,12 @@
       </div>
     </div><br>
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
       <label for="formJuniorSchool">國中學校/年級/科系</label>
         <input class="form-control" type="text" id="formJuniorSchool" name="juniorSchool"
           value="<?php echo (empty($youths)) ? "" : $youths->junior_school ?>">
       </div>
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formJuniorGraduateYear">國中畢業年度(填寫「年度」非「學年度，並請填列民國年」)</label>
           <input class="form-control" type="number" min="0" id="formJuniorGraduateYear" name="juniorGraduateYear"
           value="<?php echo (empty($youths)) ? "" : $youths->junior_graduate_year ?>"
@@ -106,7 +131,7 @@
         </div>
     </div><br>
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label>國中是否曾有中輟通報紀錄</label>
         <select class="form-select" name="juniorDropoutRecord" <?php echo (empty($youths)) ? "" : "" ?>>
           <?php if (isset($youths->junior_dropout_record)) {
@@ -124,7 +149,7 @@ if ($youths->junior_dropout_record == "1") {?>
           <?php }?>
         </select>
       </div>
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label>輔導時身分</label>
               <select class="form-select" name="counselIdentity" <?php echo (empty($youths)) ? "" : "" ?>>
                 <?php if (empty($youths->counsel_identity)) {?>
@@ -146,7 +171,7 @@ foreach ($counselIdentitys as $i) {
     </div><br>
 
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <!-- guardianName -->
         <label for="formGuardianName">監護人姓名</label>
         <input class="form-control" type="text" id="formGuardianName" name="guardianName" placeholder="OOO"
@@ -155,7 +180,7 @@ foreach ($counselIdentitys as $i) {
       </div>
 
     <!-- guardianShip -->
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formGuardianship">監護人關係</label>
         <input class="form-control" type="text" id="formGuardianship" name="guardianShip" placeholder="父子"
                 value="<?php echo (empty($youths)) ? "" : $youths->guardianship ?>"
@@ -163,7 +188,7 @@ foreach ($counselIdentitys as $i) {
       </div>
 
     <!-- guardianPhone -->
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <label for="formGuardianPhone">監護人電話</label>
         <input class="form-control" type="text" id="formGuardianPhone" name="guardianPhone" placeholder="0000-000-000"
                 value="<?php echo (empty($youths)) ? "" : $youths->guardian_phone ?>"
@@ -173,7 +198,7 @@ foreach ($counselIdentitys as $i) {
 
     <!-- guardianHouseholdAddress -->
     <div class="row">
-      <div class="col">
+      <div class="col-10 m-2 mx-auto">
         <p><label>
             <input type="checkbox" class="form-check-input" name="guardianHouseholdAddressSame" value="1"
               <?php echo (empty($youths)) ? "" : "" ?> <?php if (!empty($youths)) {if ($youths->household_address_aes == $youths->guardian_household_address_aes) {
@@ -190,7 +215,7 @@ foreach ($counselIdentitys as $i) {
 
           <!-- guardianResideAddress -->
           <div class="row">
-            <div class="col">
+            <div class="col-10 m-2 mx-auto">
               <p><label>
                   <input type="checkbox" class="form-check-input" name="guardianResideAddressSame" value="1"
                     <?php echo (empty($youths)) ? "" : "" ?> <?php if (!empty($youths)) {if ($youths->reside_address_aes == $youths->guardian_reside_address_aes) {
@@ -209,7 +234,7 @@ foreach ($counselIdentitys as $i) {
 
           <!-- source -->
           <div class="row">
-            <div class="input-field col s10 offset-m2 m8">
+          <div class="col-10 m-2 mx-auto">
               <label>青少年來源</label>
               <select class="form-select"name="source">
                 <?php if (empty($youths->source)) {?>
@@ -232,7 +257,7 @@ foreach ($sources as $i) {
 
           <!-- sourceSchoolYear -->
           <div class="row">
-            <div class="col">
+            <div class="col-10 m-2 mx-auto">
               <label for="formSourceSchoolYear">動向調查學年度</label>
               <input class="form-control"type="number" min="0" id="formSourceSchoolYear" name="sourceSchoolYear"
                 value="<?php echo (empty($youths)) ? "" : $youths->source_school_year ?>">
@@ -241,7 +266,7 @@ foreach ($sources as $i) {
 
           <!-- surveyType -->
           <div class="row">
-            <div class="col">
+            <div class="col-10 m-2 mx-auto">
               <label>動向調查類別</label>
               <select class="form-select"name="surveyType">
                 <?php if (empty($youths->survey_type)) {?>
